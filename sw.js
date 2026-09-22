a2b6c94cf7ff0fa2a79605a9b8cf0c4c50dc7d04
Quiniela Auto Sync <actions@github.com>
2026-09-22 16:14:03 +0000

    Actualización automática de la Quiniela

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="quiniela-v98";
+const CACHE="quiniela-v99";
 const CORE=["./","index.html","styles.css","app.js","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
 
 self.addEventListener("install",event=>{
