862ec003cfbe234d7acfdd20efdaea98203b7157
resuelvedudasyproblemas-rgb <resuelvedudasyproblemas@gmail.com>
2026-09-22 20:53:23 +0200

    Actualizar cache tras ajuste final del monedero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="quiniela-v126";
+const CACHE="quiniela-v127";
 const CORE=["./","index.html","styles.css","app.js","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
 
 self.addEventListener("install",event=>{
