6b34e8fda29a52fcd6c0f1666d60bdcc6a62f81b
resuelvedudasyproblemas-rgb <resuelvedudasyproblemas@gmail.com>
2026-09-22 19:57:19 +0200

    Forzar actualización del acceso al monedero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="quiniela-v115";
+const CACHE="quiniela-v116";
 const CORE=["./","index.html","styles.css","app.js","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg","icon-maskable.svg"];
 
 self.addEventListener("install",event=>{
