dff3fc801232e5fcabe8267e05a6858a8968ba25
resuelvedudasyproblemas-rgb <resuelvedudasyproblemas@gmail.com>
2026-09-25 13:43:33 +0200

    Renovar cache tras optimizar consultas

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="quiniela-v182";
-const CORE=["./","index.html","styles.css?v=172","app.js?v=181","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
+const CACHE="quiniela-v183";
+const CORE=["./","index.html","styles.css?v=172","app.js?v=182","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
