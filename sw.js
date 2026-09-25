e0898fc1297cc605476f9a970d2a8ca1b4e1c086
resuelvedudasyproblemas-rgb <resuelvedudasyproblemas@gmail.com>
2026-09-25 12:14:09 +0200

    Renovar service worker v181

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="quiniela-v180";
-const CORE=["./","index.html","styles.css?v=172","app.js?v=180","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
+const CACHE="quiniela-v181";
+const CORE=["./","index.html","styles.css?v=172","app.js?v=181","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
