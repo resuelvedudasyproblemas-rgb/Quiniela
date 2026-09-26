0251b1501325157e4417d0fa18185410febfba4c
resuelvedudasyproblemas-rgb <resuelvedudasyproblemas@gmail.com>
2026-09-26 12:49:18 +0200

    Update cache for stats and history Elige 8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="quiniela-v186";
-const CORE=["./","index.html","styles.css?v=174","app.js?v=185","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
+const CACHE="quiniela-v187";
+const CORE=["./","index.html","styles.css?v=175","app.js?v=186","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
