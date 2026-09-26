8fa90270f2fe0d9a5019eaab7876a2edb8d13ea8
resuelvedudasyproblemas-rgb <resuelvedudasyproblemas@gmail.com>
2026-09-26 21:00:51 +0200

    Update cache for Pleno live format

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="quiniela-v197";
-const CORE=["./","index.html","styles.css?v=180","app.js?v=194","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
+const CACHE="quiniela-v198";
+const CORE=["./","index.html","styles.css?v=181","app.js?v=195","config.js","manifest.webmanifest","icon-192.svg","icon-512.svg"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
